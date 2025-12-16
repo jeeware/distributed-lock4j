89270f4ecf16302f88d9473e6fed6d7b798b4293
hbourada <hichem.bourada@gmail.com>
2025-12-17 00:54:03 +0100

    fix: Enhance SimpleRetryerTest

--- src/test/java/io/github/jeeware/cloud/lock4j/support/SimpleRetryerTest.java
+++ src/test/java/io/github/jeeware/cloud/lock4j/support/SimpleRetryerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2024 Hichem BOURADA and other authors.
+ * Copyright 2020-2025 Hichem BOURADA and other authors.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,75 +17,80 @@ import com.mongodb.MongoException;
 import io.github.jeeware.cloud.lock4j.Retryer;
 import io.github.jeeware.cloud.lock4j.Retryer.Context;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.dao.DataAccessResourceFailureException;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.BindException;
 import java.net.ConnectException;
-import java.net.SocketException;
 import java.net.SocketTimeoutException;
-import java.nio.file.AccessDeniedException;
-import java.nio.file.ClosedFileSystemException;
-import java.util.Arrays;
+import java.sql.SQLTimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.from;
+import static org.springframework.beans.BeanUtils.instantiateClass;
 
 class SimpleRetryerTest {
 
-    @Test
-    void retryForExactDeclaredRetryableExceptionsShouldReturnTrue() {
-        Retryer retryer = retryer(3, IOException.class);
+    @ParameterizedTest
+    @ValueSource(classes = {SocketTimeoutException.class, SQLTimeoutException.class, FileNotFoundException.class})
+    void retryForExactRetryableExceptionShouldReturnTrue(Class<? extends Exception> retryableException) {
+        Retryer retryer = retryer(retryableException);
         Context context = retryer.createContext();
+        Exception exception = instantiateClass(retryableException);
 
-        boolean allRetry = IntStream.range(0, 3).mapToObj(i -> new IOException(String.valueOf(i)))
-                .allMatch(e -> retryer.shouldRetryFor(e, context));
+        assertThat(retryer.shouldRetryFor(exception, context)).isTrue();
+    }
 
-        assertThat(allRetry).isTrue();
-        assertThat(context.getRetryCount()).isZero();
+    @ParameterizedTest
+    @ValueSource(classes = {SocketTimeoutException.class, ConnectException.class, FileNotFoundException.class})
+    void retryForDescendentRetryableExceptionShouldReturnTrue(Class<? extends Exception> retryableException) {
+        Retryer retryer = retryer(IOException.class);
+        Context context = retryer.createContext();
+        Exception exception = instantiateClass(retryableException);
+
+        assertThat(retryer.shouldRetryFor(exception, context)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {InterruptedException.class, BindException.class, NullPointerException.class})
+    void retryForNonRetryableExceptionShouldReturnFalse(Class<? extends Exception> nonRetryableException) {
+        Retryer retryer = SimpleRetryer.builder().maxRetry(1).nonRetryableException(nonRetryableException).build();
+        Context context = retryer.createContext();
+        Exception exception = instantiateClass(nonRetryableException);
+
+        assertThat(retryer.shouldRetryFor(exception, context)).isFalse();
     }
 
     @Test
-    void retryForDescendentRetryableExceptionsShouldReturnTrue() {
-        Retryer retryer = retryer(3, IOException.class);
-        Context context = retryer.createContext();
+    void incrementRetryCountForSimpleRetryerContextWhenMaxRetryIsOneShouldReturnTerminatedTrue() {
+        Context context = SimpleRetryer.builder().maxRetry(1).build().createContext();
 
-        boolean allRetry = Stream.of(new SocketTimeoutException(), new SocketException(), new ConnectException())
-                .allMatch(e -> retryer.shouldRetryFor(e, context));
+        context.incrementRetryCount();
 
-        assertThat(allRetry).isTrue();
-    }
-
-    @Test
-    void retryForNonRetryableException() {
-        Retryer retryer = SimpleRetryer.builder().maxRetry(1).nonRetryableException(IOException.class)
-                .nonRetryableException(IllegalStateException.class).build();
-        Context context = retryer.createContext();
-
-        boolean noneMatch = Stream.of(new ClosedFileSystemException(), new AccessDeniedException(""))
-                .noneMatch(e -> retryer.shouldRetryFor(e, context));
-
-        assertThat(noneMatch).isTrue();
+        assertThat(context)
+                .returns(1, from(Context::getRetryCount))
+                .returns(true, from(Context::isTerminated));
     }
 
     @Test
     void retryForRetryableCauseShouldReturnTrue() {
-        Retryer retryer = retryer(1, IOException.class);
+        Retryer retryer = retryer(IOException.class);
         Context context = retryer.createContext();
         Exception exception = new DataAccessResourceFailureException("",
                 new MongoException("", new SocketTimeoutException("timeout")));
 
-        boolean retry = retryer.shouldRetryFor(exception, context);
-
-        assertThat(retry).isTrue();
+        assertThat(retryer.shouldRetryFor(exception, context)).isTrue();
     }
 
     @Test
     void applyWithCallableShouldSuccess() throws Exception {
         AtomicInteger counter = new AtomicInteger(0);
-        Retryer retryer = retryer(1, ArithmeticException.class);
+        Retryer retryer = retryer(ArithmeticException.class);
 
         int result = retryer.apply(() -> 1 / counter.getAndIncrement(), null);
 
@@ -95,7 +100,7 @@ class SimpleRetryerTest {
     @Test
     void applyWithCallableShouldThrow() {
         AtomicInteger counter = new AtomicInteger(0);
-        Retryer retryer = retryer(1, ArithmeticException.class);
+        Retryer retryer = retryer(ArithmeticException.class);
 
         assertThatThrownBy(() -> retryer.apply(() -> 1 / (counter.get() * (counter.getAndIncrement() - 1)), null))
                 .isInstanceOf(ArithmeticException.class)
@@ -105,16 +110,15 @@ class SimpleRetryerTest {
     @Test
     void applyWithCallableAndRecoveryShouldSuccess() throws Exception {
         AtomicInteger counter = new AtomicInteger(0);
-        Retryer retryer = retryer(1, ArithmeticException.class);
+        Retryer retryer = retryer(ArithmeticException.class);
 
         int result = retryer.apply(() -> 1 / (counter.get() * (counter.getAndIncrement() - 1)), (ex, context) -> 5);
 
         assertThat(result).isEqualTo(5);
     }
 
-    @SafeVarargs
-    private static SimpleRetryer retryer(int maxRetry, Class<? extends Exception>... retryableExceptions) {
-        return SimpleRetryer.builder().maxRetry(maxRetry).retryableExceptions(Arrays.asList(retryableExceptions)).build();
+    private static SimpleRetryer retryer(Class<? extends Exception> retryableException) {
+        return SimpleRetryer.builder().maxRetry(1).retryableException(retryableException).build();
     }
 
 }
